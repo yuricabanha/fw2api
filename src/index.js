cdfa478186839b6e056a9d78e69c3b00ffb3bbc6
Yuri Cabanha <yh.yuricabanha@gmail.com>
2025-12-05 09:33:36 -0400

    5

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-// Use "type: commonjs" in package.json to use CommonJS modules
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3000
 
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs")
+app.set("views", "./views")
 
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(bodyParser.json());
+/* estas linhas são para leitura de de body.json */
+app.use(express.json());
+app.use(express.urlencoded());
+/* aqui é o acesso exerno do sistema para a api */
 
-require('../db/dbconnect')
-require('../route/home')(app)
-require('../route/getnoticias')(app)
-require('../route/postnoticias')(app)
-require('../route/putnoticias')(app)
-require('../route/delnoticias')(app)
-require('../route/getidnoticias')(app)
-require('../route/gettiponoticia')(app)
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+//rotas
+require('../db/dbconnect')(app);
+require('../route/home')(app);
+require('../route/getnoticias')(app);
+require('../route/getidnoticias')(app);
+require('../route/gettiponoticias')(app);
+require('../route/delnoticias')(app);
+require('../route/postnoticias')(app);
+require('../route/putnoticias')(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
