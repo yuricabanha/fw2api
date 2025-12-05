17cc667355abe11604e94a9fced5d245e9ca9d48
Yuri Cabanha <yh.yuricabanha@gmail.com>
2025-12-05 09:46:36 -0400

    pelo amor de Deus n aguento mais

--- route/getnoticias.js
+++ route/getnoticias.js
@@ -2,12 +2,11 @@ module.exports = (app) => {
     app.get('/noticias', async (req, res) => {
         try {
             const resposta = await app.DBClient.connect(); //realizar a conexão com o banco 
-            res.send(resposta)
-            /* const noticias = await app.DBClient.db('portalnoticias')
+            const noticias = await app.DBClient.db('portalnoticias')
                 .collection('noticias').find().toArray();
-            res.json(noticias) */
+            res.json(noticias)
         } catch (err) {
-            res.send(" erro"+err)
+            res.send(" erro" + err)
         }
         finally {
             // Ensures that the client will close when you finish/error
